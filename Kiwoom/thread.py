cc31b836ec27538e1977fa47648071c7547ef50a
mwkim <mw.kim74@gmail.com>
2020-11-19 18:30:46 +0900

    실시간 테스트 수정

--- Kiwoom/thread.py
+++ Kiwoom/thread.py
@@ -28,15 +28,15 @@ class RealTimeScreenNumbering(QThread):
     def run(self):
         while True:
             # print("RealTimeScreenNumbering self.signal.another_job_stop : {}".format(self.signal.another_job_stop))
-            # if not self.signal.another_job_stop:
+            if not self.signal.another_job_stop:
             # self.signal.real_time_new_portfolio_fuc() # 포트폴리오 테이블에 넣을 때
-            self.signal.real_time_condition_stock_fuc()
-            self.signal.screen_number_real_time_setting()
-            QTest.qWait(1000)  # 1초
-            df_copy = self.signal.portfolio_stock_dict.copy()  # 반복문 오류를 피하기 위해
-            for code in df_copy:
-                self.signal.make_minute_candle_func(code)
-            # self.signal.another_job_stop = True
+                self.signal.real_time_condition_stock_fuc()
+                self.signal.screen_number_real_time_setting()
+            # QTest.qWait(8000)  # 8초
+            # df_copy = self.signal.portfolio_stock_dict.copy()  # 반복문 오류를 피하기 위해
+            # for code in df_copy:
+            #     self.signal.make_minute_candle_func(code)
+            self.signal.another_job_stop = True
             time.sleep(30)
 
 
